5905d6c136852d61c52b4e71f0c91fa54f2beb93
Matthew Herber <matthewherber@sbcglobal.net>
2025-01-29 12:37:59 -0500

    lab 2 implementations

---
+++ Lab 2/teleop.m
@@ -0,0 +1,45 @@
+%define our ship start spot
+xCenter = 10/2;
+yCenter = 10/2;
+radius = 1;
+
+S.ship = square(xCenter, yCenter, radius);
+%setup function callback for when keypresses occur
+set(gcf(), 'KeyPressFcn', @fig_kpfcn); 
+guidata(S.ship,S); 
+
+%This runs every keypress on teh graph
+function [] = fig_kpfcn(H,E)
+    S = guidata(H);
+    stepsize=0.1;
+    switch E.Key
+        case 'rightarrow'
+            if(get(S.ship,'XData')+stepsize < (10))
+                set(S.ship,'XData',get(S.ship,'XData')+stepsize);
+            end
+        case 'leftarrow'
+            if(get(S.ship,'XData')-stepsize > (0))
+                set(S.ship,'XData',get(S.ship,'XData')-stepsize);
+            end
+        case 'uparrow'
+            if(get(S.ship,'YData')+stepsize < (10))
+                set(S.ship,'YData',get(S.ship,'YData')+stepsize);
+            end
+        case 'downarrow'
+            if(get(S.ship,'YData')-stepsize > (0))
+                set(S.ship,'YData',get(S.ship,'YData')-stepsize);
+            end
+    end
+end
+
+%this runs everytime the graph is updated
+function h = square(xCenter,yCenter,radius)
+    hold on
+    xlim([0,10]);
+    ylim([0,10]);
+    %draw the square at x and y center
+    h = plot(xCenter,yCenter, "squarer");
+    %set the marker size so the square is the correct size
+    h.MarkerSize = radius * 5;
+    hold off
+end
